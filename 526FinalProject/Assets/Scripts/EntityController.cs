526b35707444e9aba9291d0bd02fd173aa159f25
Masato Lin <masatoli@usc.edu>
2023-09-30 11:12:32 -0700

    added in personal scene and set up for entity controller

---
+++ 526FinalProject/Assets/Scripts/EntityController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityController : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 50.0f;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private bool allowVerticalMovement = false;
+    
+    private Rigidbody2D rb;
+    void Start()
+    {
+        //initialize rigidbody
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //function for when the player first initially possesses entity
+    public virtual void Possess()
+    {
+        
+    }
+
+    //function that's called by player if they possess the player, can be overridden
+    public virtual void Move(float horizontal, float vertical, bool jumpPressed)
+    {
+        //movement
+        Vector2 movement = new Vector2(horizontal, 0.0f);
+        
+        //use vertical movement if checked
+        if (allowVerticalMovement)
+        {
+            movement.y = vertical;
+        }
+        
+        //move rigidbody
+        rb.position += movement * speed * Time.deltaTime;
+        
+        //jump
+        if (jumpPressed)
+        {
+            rb.AddForce(new Vector2(0.0f, jumpForce));
+        }
+    }
+}
