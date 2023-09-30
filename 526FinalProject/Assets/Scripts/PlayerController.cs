865b61b0051cfa19cb60b01f22f967555dc9b529
Aaron Ahmed <aaron.ahmed71@gmail.com>
2023-09-29 17:34:02 -0700

    Started player prefab

---
+++ 526FinalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float forwardInput;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
