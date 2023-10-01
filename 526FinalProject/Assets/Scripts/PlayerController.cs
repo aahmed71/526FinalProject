2b589f3d31909e29013339ecc828275f31d6d658
Aaron Ahmed <aaron.ahmed71@gmail.com>
2023-10-01 14:22:36 -0700

    Added player movement

--- 526FinalProject/Assets/Scripts/PlayerController.cs
+++ 526FinalProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
-    private float forwardInput;
+    private float verticalInput;
+
+    [SerializeField] private float speed;
     private Rigidbody2D rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput, verticalInput) * speed;
     }
 }
