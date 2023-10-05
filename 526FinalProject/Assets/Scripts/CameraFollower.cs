4a8478979173eeddf0f132258f2e7840effcf097
Riddhi Hakani <rhakani@usc.edu>
2023-10-04 20:18:45 -0700

    Added Player Follower Script

---
+++ 526FinalProject/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    
+    public Transform target; 
+    public float smoothSpeed = 0.125f; 
+    public Vector3 offset = new Vector3(0f, 2f, -10f); 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
--- 526FinalProject/Assets/Scripts/EnemyMotion.cs
+++ 526FinalProject/Assets/Scripts/EnemyMotion.cs
@@ -5,39 +5,37 @@ using UnityEngine;
 public class EnemyMotion : MonoBehaviour
 
 {
-    public float moveSpeed = 2.0f; // Adjust the speed as needed
-    public float changeInterval = 2.0f; // Time interval for changing direction
+    public float moveSpeed = 2.0f; 
+    public float changeInterval = 2.0f; 
     public Vector2 minBounds;
     public Vector2 maxBounds;
 
     private float timer;
-    private Vector2 randomDirection;// Start is called before the first frame update
+    private Vector2 randomDirection;
     void Start()
     {
         timer = changeInterval;
         randomDirection = Random.insideUnitCircle.normalized;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         timer -= Time.deltaTime;
-
-        // If it's time to change direction, choose a new random direction
+ 
         if (timer <= 0)
         {
             randomDirection = Random.insideUnitCircle.normalized;
             timer = changeInterval;
         }
 
-        // Calculate the new position
+        
         Vector2 newPosition = (Vector2)transform.position + randomDirection * moveSpeed * Time.deltaTime;
 
-        // Clamp the new position to stay within the defined bounds
+       
         newPosition.x = Mathf.Clamp(newPosition.x, minBounds.x, maxBounds.x);
         newPosition.y = Mathf.Clamp(newPosition.y, minBounds.y, maxBounds.y);
 
-        // Move the object to the clamped position
         GetComponent<Rigidbody2D>().MovePosition(newPosition);
     }
 }
