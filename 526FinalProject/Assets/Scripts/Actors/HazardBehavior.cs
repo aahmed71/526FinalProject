2a6825d55a743049b52b9aee22f2bba48809e402
Aryan Methil <aryanmethil@gmail.com>
2023-10-13 23:22:51 -0700

    Added hazard prefab & mechanics
    - Hazard will roam between points
    - Hazard touches ghost => ghost dies
    - Hazard touches entity that is possessed by a ghost => ghost
      unpossesses
    - Grace period of x seconds after being unpossessed
    	- To avoid the case where ghost gets killed after possessing
    	  something because the position where hazard was supposed to
    	  be in the next frame, is where the ghost spawns after
    	  unpossessing

---
+++ 526FinalProject/Assets/Scripts/Actors/HazardBehavior.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] pathArray;
+    private int destinationIdx;
+    private int totalPoints;
+    public float speed = 5.0f;
+    private Vector3 directionToDestination;
+    public GameObject player;
+
+    // time for which the ghost enjoys invincibility after getting unpossessed by the hazard
+    public float invincibilityDuration = 1.5f;
+    // current timer for the invincible period
+    private float secondChanceTimer = 0.0f;
+    // if invincible mode is enabled
+    private int secondChanceFlag = 0;
+
+    void Start()
+    {
+        totalPoints = pathArray.Length;
+        destinationIdx = Mathf.Min(1,totalPoints-1);
+        if(totalPoints>0)
+        {
+            transform.position = pathArray[0].position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if second chance invincibility is enabled, if yes then maintain the timer
+        if(secondChanceFlag==1 && secondChanceTimer<invincibilityDuration)
+        {
+            secondChanceTimer+=Time.deltaTime;
+        }
+        else
+        {
+            secondChanceFlag=0;
+            secondChanceTimer=0.0f;
+        }
+
+        directionToDestination = pathArray[destinationIdx].position-transform.position;
+        if(directionToDestination.magnitude > 1)
+        {
+            directionToDestination.Normalize();
+            transform.position += directionToDestination * speed * Time.deltaTime;
+        }
+        else
+        {
+            destinationIdx = (destinationIdx+1)%totalPoints;
+        }
+    }
+     
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // check if the ghost was recently unpossessed by a hazard and because of it, is currently in an invincible period duration.
+        if(collision.gameObject.CompareTag("Player") && secondChanceFlag==0)
+        {
+            Destroy(collision.gameObject);
+        }
+
+        //check if the entity that we are colliding with is the same entity that the player is currently possessing.
+        else if(collision.gameObject.CompareTag("Entity") && player!=null && player.GetComponent<PlayerController>().currentEntity == collision.gameObject.GetComponent<EntityController>())
+        {
+            player.GetComponent<PlayerController>().UnPossess();
+
+            //enable second chance invincibility
+            secondChanceFlag=1;
+        }
+
+    }
+}
