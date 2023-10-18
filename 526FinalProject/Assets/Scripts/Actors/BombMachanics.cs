c94fcef37d574cf84da924fc3d2a13b73b37cc08
Masato Lin <masatoli@usc.edu>
2023-10-18 14:10:55 -0700

    adjusted bomb animation

--- 526FinalProject/Assets/Scripts/Actors/BombMachanics.cs
+++ 526FinalProject/Assets/Scripts/Actors/BombMachanics.cs
@@ -39,7 +39,7 @@ public class BombMechanics : EntityController
         }
         
         canBePossessed = false;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1f);
         Destroy(gameObject);
     }
 
