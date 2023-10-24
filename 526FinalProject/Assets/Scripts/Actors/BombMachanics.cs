5a29a92423f4378617e997932d42b3a77cc7dbd8
Masato Lin <masatoli@usc.edu>
2023-10-23 23:35:11 -0700

    finished tweaking bomb

--- 526FinalProject/Assets/Scripts/Actors/BombMachanics.cs
+++ 526FinalProject/Assets/Scripts/Actors/BombMachanics.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class BombMechanics : EntityController
 {
@@ -15,25 +16,20 @@ public class BombMechanics : EntityController
        StartCoroutine(DestroyBombWithDelay());
     }
 
+    private void Start()
+    {
+        _explosion.SetActive(false);
+    }
 
     private IEnumerator DestroyBombWithDelay()
     {
         _animator.SetTrigger("StartExplosion");
         yield return new WaitForSeconds(2.5f);
 
+        //expand explosion
+        _explosion.SetActive(true);
         StartCoroutine(LerpScale(explosionRadius, explosionSpeed));
         yield return new WaitForSeconds(explosionSpeed);
-        //detects all colliders within the explosion radius
-        Collider2D[] objectsInRange = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
-
-        foreach (Collider2D col in objectsInRange)
-
-        {
-            if (col.CompareTag("Hazard") || col.CompareTag("Door") || col.CompareTag("Barrier"))
-            {
-                Destroy(col.gameObject);
-            }
-        }
 
         //unpossess player if possessed
         if (playerRef)
@@ -48,18 +44,20 @@ public class BombMechanics : EntityController
     
     IEnumerator LerpScale(float end, float lerpSpeed)
     {
+        //expands size over time
         float timer = 0;
         while(timer < 1)
         {
             float scale = Mathf.Lerp(0.0f, end, timer);
             _explosion.transform.localScale = new Vector3(scale, scale, 1.0f);
-            
+            _explosion.GetComponent<CircleCollider2D>().radius = scale;
 
             timer = timer + Time.deltaTime / lerpSpeed;
             yield return new WaitForEndOfFrame();
         }
 
         _explosion.transform.localScale = new Vector3(end, end, 1.0f);
+        _explosion.GetComponent<CircleCollider2D>().radius = end;
         
     }
 
